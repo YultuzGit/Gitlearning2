5c5064c13cf1aee3a08f3bf475bde3a1def08dff
YultuzGit <yultuzallinone@gmail.com>
2022-11-20 21:23:21 -0500

    second commit

---
+++ src/main/main/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Flat_A1 {
+
+
+
+
+
+
+    public static void main(String args[]) throws Exception{
+
+        Flat_B1 obj_b1 = new Flat_B1();
+        System.out.println(obj_b1.bankInterestrate("SBI"));
+        System.out.println("Now I am the owner of all Google share which my parents owned:"+ obj_b1.FatherisHavingsomeshare_Google); // Variables ....
+
+
+    }
+
+
+}
+
+
+
+
+}
